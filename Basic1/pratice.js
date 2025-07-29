ba0816901d0f625802c15e0582cdc58f71841461
Prashant730 <1306pkumar@gmail.com>
2025-07-29 16:57:18 +0530

    pratice question

--- Basic1/pratice.js
+++ Basic1/pratice.js
@@ -13,3 +13,25 @@ console.table(Student)
 console.log(typeof Student)
 console.log(typeof Student.fullName)
 console.log(typeof Student.age)
+
+const pen = {
+  colorOfPen: 'black',
+  colorOfInk: 'blue',
+  typeOfPen: 'ballPen',
+  priceOfPen: 'Rs10',
+}
+console.table(pen)
+
+console.log(pen.typeOfPen)
+console.log(pen.colorOfPen)
+console.log(pen.colorOfInk)
+console.log(pen.priceOfPen)
+
+console.log()
+
+console.log(pen.colorofInk) //undefined
+console.log(pen.priceofPen) //undefined
+
+console.log()
+
+console.log(typeof pen)
