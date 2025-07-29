99b53dd374f245201f6babf32a97675f917cc87b
Prashant730 <1306pkumar@gmail.com>
2025-07-29 16:44:38 +0530

    add file pratice

---
+++ Basic1/pratice.js
@@ -0,0 +1,15 @@
+const Student = {
+  fullName: 'Prashant',
+  age: 18,
+}
+console.log(Student.fullName)
+console.log(Student.age)
+
+console.log(Student['fullName'])
+console.log(Student['age'])
+
+console.table(Student)
+
+console.log(typeof Student)
+console.log(typeof Student.fullName)
+console.log(typeof Student.age)
