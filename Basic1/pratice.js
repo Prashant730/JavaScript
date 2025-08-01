c7590dc63963b31fc4979020db22ce02c7ba0e87
Prashant730 <1306pkumar@gmail.com>
2025-08-02 01:25:47 +0530

    pratice question

--- Basic1/pratice.js
+++ Basic1/pratice.js
@@ -45,3 +45,27 @@ const student = {
 // student.fullName is a property of the student object, not a variable
 console.log(student.fullname)
 console.log(student.fullName)
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Conditional statements
+
+// you use another values to check if a condition is true or false for (number and score variables)
+let number = 25
+if (number % 5 === 0) {
+  console.log('number is multiple of 5')
+} else {
+  console.log('number is not a multiple of 5')
+}
+
+let score = 90
+if (score >= 90 && score <= 100) {
+  console.log('Grade A')
+} else if (score >= 70 && score <= 89) {
+  console.log('Grade B')
+} else if (score >= 60 && score <= 69) {
+  console.log('Grade C')
+} else if (score >= 50 && score <= 59) {
+  console.log('Grade D')
+} else {
+  console.log('Grade E')
+}
