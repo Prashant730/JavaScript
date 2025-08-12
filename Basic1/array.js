00ea50341ee1e599c853e5bebd591a60cfec2a4b
Prashant730 <1306pkumar@gmail.com>
2025-08-13 00:13:57 +0530

    array method added

--- Basic1/array.js
+++ Basic1/array.js
@@ -1,55 +1,55 @@
-// //Array
-// // Array is a collection of items with a common type
-// // It is a special type of object that allows you to store multiple values in a single variable
-// // object --> key-value pairs(key.value) , array --> index-value pairs(index.value)
-// //array has a object data type
+//Array
+// Array is a collection of items with a common type
+// It is a special type of object that allows you to store multiple values in a single variable
+// object --> key-value pairs(key.value) , array --> index-value pairs(index.value)
+//array has a object data type
 
-// // example:
-// // let arr = [1, 2, 3, 4, 5];// array of numbers
-// // let arr2 = ['a', 'b', 'c', 'd'];// array of strings
-// // let arr3 = [1, 'a', true, null, undefined];// array of mixed types but not recommended
+// example:
+// let arr = [1, 2, 3, 4, 5];// array of numbers
+// let arr2 = ['a', 'b', 'c', 'd'];// array of strings
+// let arr3 = [1, 'a', true, null, undefined];// array of mixed types but not recommended
 
-// let name = [
-//   'Prashant',
-//   'Arsh',
-//   'Aryan',
-//   'Surayanh',
-//   'Kushagra',
-//   'Abhishek',
-//   'Samy',
-//   'Shubham',
-// ]
+let name = [
+  'Prashant',
+  'Arsh',
+  'Aryan',
+  'Surayanh',
+  'Kushagra',
+  'Abhishek',
+  'Samy',
+  'Shubham',
+]
 
-// //if we want to access any element of the array we can use access form it's index and index starts from 0
-// console.log(name[0])
-// console.log(name[1])
-// console.log(name[2])
-// console.log(name[3])
-// console.log(name[4])
-// console.log(name[5])
-// console.log(name[6])
-// console.log(name[7])
+//if we want to access any element of the array we can use access form it's index and index starts from 0
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+console.log(name[5])
+console.log(name[6])
+console.log(name[7])
 
-// //to calculate the length of the array we can use the length property  that is arr.length
-// console.log(name.length) //8
+//to calculate the length of the array we can use the length property  that is arr.length
+console.log(name.length) //8
 
-// name[0] = 'Prashant Kumar' //updating the value inside the array
-// console.log(name[0]) //Prashant Kumar
+name[0] = 'Prashant Kumar' //updating the value inside the array
+console.log(name[0]) //Prashant Kumar
 
-// //how to print the whole array by using for loop
-// for (let i = 0; i < name.length; i++) {
-//   console.log(name[i])
-// }
+//how to print the whole array by using for loop
+for (let i = 0; i < name.length; i++) {
+  console.log(name[i])
+}
 
-// //how to print the whole array by using for-of loop
-// for (let i of name) {
-//   console.log(i)
-// }
+//how to print the whole array by using for-of loop
+for (let i of name) {
+  console.log(i)
+}
 
-// //error: name[i] is not defined, because i is a number, not an index
-// for (let i of name) {
-//   console.log(name[i]) //undefined
-// }
+//error: name[i] is not defined, because i is a number, not an index
+for (let i of name) {
+  console.log(name[i]) //undefined
+}
 
 //-------------------------------------------------------------------------------------------------------------------------
 
@@ -98,4 +98,13 @@ console.log(array.slice(-5, 1))
 console.log(array.slice(-5, -1))
 console.log(array.slice(-5))
 
-//
+//splice --> splice(starIndex , no of elements to delete , elements to add)
+let arr11 = [1, 2, 3, 4, 5]
+console.log(arr11.splice(2, 2, 90, 89, 88))
+console.log(arr11)
+console.log(arr11.splice())
+console.log(arr11) //no change
+console.log(arr11.splice(1, 1, 3))
+console.log(arr11) //2-->3
+console.log(arr11.splice(1, 1))
+console.log(arr11) //3 will be deleted
