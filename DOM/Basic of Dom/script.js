ab5937cb7ed44bc4f170510d55cf7afc8c1580e8
Prashant730 <1306pkumar@gmail.com>
2025-10-06 11:06:31 +0530

    Start of DOM

---
+++ DOM/Basic of Dom/script.js
@@ -0,0 +1,10 @@
+let el = document.querySelectorAll('div')
+el[0].innerText = 'Hello World!'
+el[1].innerText = 'Hello World!'
+el[2].innerText = 'Hello World!'
+console.log(el)
+
+let h2 = document.querySelector('h2')
+console.log(h2.innerText)
+h2.innerText = h2.innerText + ' from college'
+console.log(h2.innerText)
